0810c75e3343140f4f9dd23dbe3bb36fcfaed1ac
Debora Souza <debora.souzalima22@gmail.com>
2025-02-22 17:16:32 -0300

    Correção de bug ao HomeController ser chamado, incluindo a função tratarDados das datas.

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,30 +1,39 @@
 angular.module('meuApp')
-.controller('HomeController', function($scope, $http, $state){
-    console.log('HomeController funcionou!');
-    $token = localStorage.getItem('token');
-    $config = {
-        headers: {
-            'Authorization': 'Bearer ' + $token
+    .controller('HomeController', function ($scope, $http, $state) {
+        console.log('HomeController funcionou!');
+        $token = localStorage.getItem('token');
+        $config = {
+            headers: {
+                'Authorization': 'Bearer ' + $token
+            }
         }
-    }
 
-    logout = function(){
-        localStorage.removeItem('token');
-        $state.go('login');
-    }
+        logout = function () {
+            localStorage.removeItem('token');
+            $state.go('login');
+        }
 
-    $scope.listar = function () {
-        $http.get('http://localhost:8000/api/projetos/listar', $config).then(function (response) {
-            if (response.status == 200) {
-                $scope.projetos = tratarDados(response.data);
+        $scope.listar = function () {
+            $http.get('http://localhost:8000/api/projetos/listar', $config).then(function (response) {
+                if (response.status == 200) {
+                    $scope.projetos = tratarDados(response.data);
+                }
+            }, function (error) {
+                if (error.status == 401) {
+                    logout();
+                }
+                console.log(error);
+            });
+        }
+
+        $scope.listar();
+
+        // função que converte as datas
+        tratarDados = function (dados) {
+            for (x = 0; x < dados.length; x++) {
+                dados[x]['dataDeInicio'] = new Date(dados[x]['dataDeInicio']);
+                dados[x]['dataDeConclusao'] = new Date(dados[x]['dataDeConclusao']);
             }
-        }, function (error) {
-            if(error.status == 401){
-                logout();
-            }
-            console.log(error);
-        });
-    }
-
-    $scope.listar();
-});
+            return dados;
+        }
+    });
